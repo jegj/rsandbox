c4c0c5eb54e2c580796d8df108ad01d62794bf0b
Javier Galarza <jegj57@gmail.com>
2024-02-16 06:17:26 -0500

    chore: submit anagram passed version

--- exercism/anagram/src/lib.rs
+++ exercism/anagram/src/lib.rs
@@ -1,6 +1,6 @@
 use std::collections::HashSet;
 
-fn sort_string(s: &str) -> String {
+fn sort_string(s: String) -> String {
     let mut chars: Vec<char> = s.chars().collect();
     chars.sort();
     chars.into_iter().collect()
@@ -8,12 +8,12 @@ fn sort_string(s: &str) -> String {
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &'a [&'a str]) -> HashSet<&'a str> {
     let mut anagrams = HashSet::new();
-    let sorted_word = sort_string(word);
+    let sorted_word = sort_string(word.to_lowercase());
 
     for possible_word in possible_anagrams {
-        let sorted_possible_word = sort_string(possible_word);
+        let sorted_possible_word = sort_string(possible_word.to_lowercase());
         if sorted_possible_word.to_lowercase() == sorted_word.to_lowercase()
-            && *possible_word != word
+            && *possible_word.to_lowercase() != word.to_lowercase()
         {
             anagrams.insert(*possible_word);
         }
