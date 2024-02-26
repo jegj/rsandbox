9ed33f495c03236d5d53f01f3585a98a3351dbcb
Javier Galarza <jegj57@gmail.com>
2024-02-26 05:24:07 -0500

    chore: playing around

--- exercism/minesweeper/src/lib.rs
+++ exercism/minesweeper/src/lib.rs
@@ -2,7 +2,17 @@ pub fn annotate(minefield: &[&str]) -> Vec<String> {
     let columns: usize = minefield.len();
     let rows: usize = minefield[0].len();
 
-    let matrix: Vec<Vec<u32>> = vec![vec![0; columns]; rows];
+    let mut matrix: Vec<Vec<u32>> = vec![vec![0; columns]; rows];
+
+    for x in 0..minefield.len() {
+        let row = minefield[x];
+        let mut y = 0;
+        for column in row.as_bytes() {
+            let column_char = (*column as char).to_string();
+            println!("row {} column==>{}", x, column_char);
+            y += 1;
+        }
+    }
 
     // Transform the matrix into a vector of strings
     let matrix_as_strings: Vec<String> = matrix
